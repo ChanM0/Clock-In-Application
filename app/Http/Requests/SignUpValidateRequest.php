ca64a621d5d264c9b847be126bf58c329357326e
chanM0 <mikec201722@gmail.com>
2018-11-30 00:05:00 -0800

    Able to sign into users and login - set migrations

--- app/Http/Requests/SignUpValidateRequest.php
+++ app/Http/Requests/SignUpValidateRequest.php
@@ -24,10 +24,29 @@ class SignUpValidateRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
-            // 'password' => 'required|confirmed'
+            'first_name' => 'required|min:3|max:255',
+            'last_name' => 'required|min:3|max:255',
+            'password' => 'required|min:3',
+            // 'password' => 'required|min:3|confirmed',
+            'email' => 'required|unique:users,email',
         ];
+    }
+
+    public function message()
+    {
+        return [
+            'first_name.required' => 'First Name is required.',
+            'first_name.min' => 'First Name must be at least three characters long.',
+            'first_name.max' => 'First Name must be at least three characters long.',
+            'last_name.required' => 'Last Name is required.',
+            'last_name.min' => 'Last Name must be at least three characters long.',
+            'last_name.max' => 'Last Name must be at least three characters long.',
+            'password.required' => 'Email is required.',
+            'password.min' => 'Email must be more than 3 characters long.',
+            'email.required' => 'Email is required.',
+            'email.email_address' => 'Not in an email address format.',
+            'email.unique' => 'Email has already been registered.'
+        ];
+
     }
 }
