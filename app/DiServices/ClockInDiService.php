3f0c0f5b0b70615057f8ae5d7a1ed0a30fafd8ac
chanM0 <mikec201722@gmail.com>
2018-11-29 22:36:32 -0800

    Clock in - controller - factory - model - Dependency Injection Interface - Service

---
+++ app/DiServices/ClockInDiService.php
@@ -0,0 +1,70 @@
+<?php 
+namespace App\Services;
+
+use Carbon\Carbon;
+
+use App\ClockIn;
+use App\User;
+
+use App\DiInterfaces\ClockInDiInterface;
+
+
+class ClockInDiService implements ClockInDiInterface
+{
+  private function getCurrentDay()
+  {
+    $carbonDate = Carbon::now();
+    $date = Carbon::parse($carbonDate)->format('d/m/Y');
+    return $date;
+  }
+
+  public function checkIn($dataArray)
+  {
+    $dayOf = $this->getCurrentDay();
+
+    $userCheckIn = new ClockIn;
+    $userCheckIn->user_id = $dataArray['user_id'];
+    $userCheckIn->time_in = $dataArray['time_in'];
+    $userCheckIn->day_of = $dayOf;
+    $userCheckIn->save();
+
+    return response('Check In Successful', 200);
+  }
+
+  public function checkOut($dataArray)
+  {
+    $dayOf = $this->getCurrentDay();
+
+    // catch exception;
+    $userCheckOut = ClockIn::where('user_id', $dataArray['user_id'])->where('day_of', $dayOf)->firstOrFail();
+
+    $userCheckOut->time_out = $dataArray['time_out'];
+
+    $userCheckOut->save();
+
+    return response('Check Out Successful', 200);
+  }
+
+    // Admin Methods
+  public function getAllUsersLogs($dataArray)
+  {
+    $dataArray['day_of'] = $request->day_of;
+    $dataArray['user_id'] = $request->user_id;
+
+    // Relationship 
+    $userLogs = User::where('id', $dataArray['user_id'])->with('hasManyLogs')->get();
+
+    return $userLogs;
+  }
+
+  public function getAllLogsOnThisDay($dataArray)
+  {
+    $userLogs = ClockIn::where('day_of', $dataArray['day_of'])->get();
+
+    if ($userLogs == null || isEmpty($userLogs)) {
+      return response('No data found', 200);
+    }
+
+    return $userLogs;
+  }
+}
---
+++ database/migrations/2018_11_30_061341_create_clock_ins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClockInsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clock_ins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('time_in');
+            $table->dateTime('time_out')->nullable();
+            $table->date('day_of');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clock_ins');
+    }
+}
